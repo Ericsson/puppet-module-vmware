61b74697ae047ec9cad5b1eedc7b18c40136a976
Phil Friderici <phil.friderici@ericsson.com>
2017-07-05 16:53:37 +0200

    Rewrite tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,443 @@ require 'spec_helper'
 describe 'vmware' do
   let(:default_facts) do
     {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'RedHat',
+      :operatingsystemrelease => '6.0',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.32-431.11.2.el6.x86_64',
+      :virtual                => 'vmware',
+      :vmware_has_x           => 'false',
+    }
+  end
+  let(:facts) { default_facts }
+
+####
+## show resources in cataloge:
+# it { pp catalogue.resources }
+####
+
+  platforms = {
+    'CentOS 5.10 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'CentOS',
+      :operatingsystemrelease => '5.10',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.18-371.el5',
+      :_service_path          => '/etc/init.d/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => false,
+    },
+    'CentOS 6.5 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'CentOS',
+      :operatingsystemrelease => '6.5',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.32-431.el6.x86_64',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => false,
+    },
+    'CentOS 7.0 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'CentOS',
+      :operatingsystemrelease => '7.0.1406',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.10.0-123.el7.x86_64',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+    'OpenSuSE 12.3 x86_64' => {
+      :osfamily               => 'Suse',
+      :operatingsystem        => 'OpenSuSE',
+      :operatingsystemrelease => '12.3',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.7.10-1.36-desktop',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+    'RedHat 5.10 i386' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'RedHat',
+      :operatingsystemrelease => '5.10',
+      :architecture           => 'i386',
+      :kernelrelease          => '2.6.18-371.el5',
+      :_service_path          => '/etc/init.d/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => false,
+    },
+    'RedHat 5.10 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'RedHat',
+      :operatingsystemrelease => '5.10',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.18-371.el5',
+      :_service_path          => '/etc/init.d/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => false,
+    },
+    'RedHat 6.5 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'RedHat',
+      :operatingsystemrelease => '6.5',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.32-431.el6.x86_64',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => false,
+    },
+    'RedHat 7.0 x86_64' => {
+      :osfamily               => 'RedHat',
+      :operatingsystem        => 'RedHat',
+      :operatingsystemrelease => '7.0.1406',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.10.0-123.el7.x86_64',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+    # no need to test SLED as the module treats it equal to SLES
+    'SLES 11.1 i386' => {
+      :osfamily               => 'Suse',
+      :operatingsystem        => 'SLES',
+      :operatingsystemrelease => '11.1',
+      :architecture           => 'i386',
+      :kernelrelease          => '2.6.32.12-0.7-default',
+      :_service_path          => '/etc/init.d/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => false,
+    },
+    'SLES 11.1 x86_64' => {
+      :osfamily               => 'Suse',
+      :operatingsystem        => 'SLES',
+      :operatingsystemrelease => '11.1',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.32.12-0.7-default',
+      :_service_path          => '/etc/init.d/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => false,
+    },
+    'SLES 11.4 x86_64' => {
+      :osfamily               => 'Suse',
+      :operatingsystem        => 'SLES',
+      :operatingsystemrelease => '11.4',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '2.6.32.12-0.7-default',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => false,
+      :_use_open_vm_tools     => true,
+    },
+    'SLES 12.1 x86_64' => {
+      :osfamily               => 'Suse',
+      :operatingsystem        => 'SLES',
+      :operatingsystemrelease => '12.1',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.12.62-60.64.8-default',
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+    'Ubuntu 12.04 x86_64' => {
+      :osfamily               => 'Debian',
+      :operatingsystem        => 'Ubuntu',
+      :operatingsystemrelease => '12.04',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.5.0-23-generic',
+      :lsbdistcodename        => 'precise', # needed for apt
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+    'Ubuntu 14.04 x86_64' => {
+      :osfamily               => 'Debian',
+      :operatingsystem        => 'Ubuntu',
+      :operatingsystemrelease => '14.04',
+      :architecture           => 'x86_64',
+      :kernelrelease          => '3.16.0-30-generic',
+      :lsbdistcodename        => 'trusty', # needed for apt
+      :_service_path          => '/etc/vmware-tools/init/',
+      :_enableSyncDriver      => true,
+      :_use_open_vm_tools     => true,
+    },
+  }
+
+  platforms.sort.each do |platform, v|
+    describe "with default values for all parameters on #{platform} on a vmware hypervisor without X installed" do
+      let(:facts) do
+        {
+          :osfamily               => v[:osfamily],
+          :operatingsystem        => v[:operatingsystem],
+          :operatingsystemrelease => v[:operatingsystemrelease],
+          :architecture           => v[:architecture],
+          :kernelrelease          => v[:kernelrelease],
+          :lsbdistcodename        => v[:lsbdistcodename],
+          :virtual                => 'vmware',
+          :vmware_has_x           => 'false',
+        }
+      end
+
+      if v[:_use_open_vm_tools]
+        _vmtools_package  = 'open-vm-tools'
+        if v[:operatingsystem] == 'Ubuntu'
+          _service_name = 'open-vm-tools'
+        else
+          _service_name = 'vmtoolsd'
+        end
+        _service_provider = nil
+        _service_path     = nil
+        _resource_count   = 6
+        _class_count      = 1
+      else
+        _vmtools_package  = 'vmware-tools-esx-nox'
+        _service_name     = 'vmware-tools-services'
+        _service_provider = 'init'
+        _service_path     = v[:_service_path]
+        _resource_count   = 7
+        if v[:osfamily] =~ /(Suse|Ubuntu)/
+          _class_count = 2
+        else
+          _class_count = 1
+        end
+      end
+
+      it { should compile.with_all_deps }
+      it { should have_class_resource_count(_class_count) }
+      it { should have_resource_count(_resource_count) }
+      it { should contain_class('vmware')}
+
+      case v[:operatingsystem]
+      when 'RedHat', 'CentOS'
+        if v[:_use_open_vm_tools]
+          it { should have_yumrepo_resource_count(0) }
+        else
+          it { should have_yumrepo_resource_count(1) }
+          it do
+            should contain_yumrepo('vmware-osps').with({
+              'baseurl'  => "http://packages.vmware.com/tools/esx/latest/rhel#{v[:operatingsystemrelease].partition('.').first}/#{v[:architecture]}",
+              'enabled'  => '1',
+              'gpgcheck' => '1',
+              'gpgkey'   => 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub',
+              'proxy'    => nil,
+            })
+          end
+        end
+        it { should_not contain_class('zyyprepo')}
+        it { should have_zypprepo_resource_count(0) }
+        it { should_not contain_class('apt')}
+        it { should have_apt__key_resource_count(0) }
+        it { should have_apt__source_resource_count(0) }
+      when 'SLED', 'SLES', 'OpenSuSE'
+        if v[:_use_open_vm_tools]
+          it { should_not contain_class('zyyprepo')}
+          it { should have_zypprepo_resource_count(0) }
+        else
+          it { should contain_class('zypprepo')}
+          it { should have_zypprepo_resource_count(1) }
+
+          it do
+            should contain_zypprepo('vmware-osps').with({
+              'enabled'     => 1,
+              'autorefresh' => 0,
+              'path'        => '/',
+              'type'        => 'yum',
+              'gpgcheck'    => 1,
+              'gpgkey'      => 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub',
+            })
+          end
+
+          if v[:architecture] == 'x86_64'
+            it { should contain_zypprepo('vmware-osps').with_baseurl("http://packages.vmware.com/tools/esx/latest/sles#{v[:operatingsystemrelease]}/x86_64") }
+          else
+            it { should contain_zypprepo('vmware-osps').with_baseurl("http://packages.vmware.com/tools/esx/latest/sles#{v[:operatingsystemrelease]}/i586") }
+          end
+        end
+        it { should have_yumrepo_resource_count(0) }
+        it { should_not contain_class('apt')}
+        it { should have_apt__key_resource_count(0) }
+        it { should have_apt__source_resource_count(0) }
+      when 'Ubuntu'
+        if v[:_use_open_vm_tools]
+          it { should_not contain_class('apt')}
+          it { should have_apt__key_resource_count(0) }
+          it { should have_apt__source_resource_count(0) }
+        else
+          it { should contain_class('apt')}
+          it { should have_apt__key_resource_count(1) }
+          it do
+            should contain_apt_key('vmware').with({
+              'key'        => 'C0B5E0AB66FD4949',
+              'key_source' => 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub',
+            })
+          end
+          it { should have_apt__source_resource_count(1) }
+          it do
+            should contain_apt__source('vmware-osps').with({
+              'location'    => %r{http://packages.vmware.com/tools/esx/latest/ubuntu},
+              'release'     => v[:lsbdistcodename],
+              'repos'       => 'main',
+              'include_src' => false,
+            })
+          end
+        end
+        it { should have_yumrepo_resource_count(0) }
+        it { should_not contain_class('zyyprepo')}
+        it { should have_zypprepo_resource_count(0) }
+      end
+
+      it { should have_exec_resource_count(1) }
+      it do
+        should contain_exec('Remove vmware tools script installation').with({
+          'command' => 'installer.sh uninstall',
+          'path'    => '/usr/bin/:/etc/vmware-tools/',
+          'onlyif'  => 'test -e "/etc/vmware-tools/locations" -a ! -e "/usr/lib/vmware-tools/dsp"',
+          'before'  => [ "Package[#{_vmtools_package}]" ],
+        })
+      end
+
+      it { should have_package_resource_count(1) }
+      it { should contain_package("#{_vmtools_package}").with('ensure' => 'present') }
+
+      it { should have_service_resource_count(1) }
+      it do
+        should contain_service(_service_name).with({
+          'provider' => _service_provider,
+          'path'     => _service_path,
+          'ensure'   => 'running',
+          'require'  => "Package[#{_vmtools_package}]",
+        })
+      end
+
+      it { should have_file_resource_count(1) }
+      it do
+        should contain_file('vmtools_conf').with({
+          'ensure'  => 'file',
+          'path'    => '/etc/vmware-tools/tools.conf',
+          'require'  => "Package[#{_vmtools_package}]",
+        })
+      end
+
+      it { should have_ini_setting_resource_count(2) }
+      it do
+        should contain_ini_setting('[vmtools] disable-tools-version').with({
+          'ensure'  => 'present',
+          'path'    => '/etc/vmware-tools/tools.conf',
+          'section' => 'vmtools',
+          'value'   => 'true',
+          'notify'  => "Service[#{_service_name}]",
+          'require' => 'File[vmtools_conf]',
+        })
+      end
+
+      it do
+        should contain_ini_setting('[vmbackup] enableSyncDriver').with({
+          'ensure'  => 'present',
+          'path'    => '/etc/vmware-tools/tools.conf',
+          'section' => 'vmbackup',
+          'value'   => v[:_enableSyncDriver],
+          'notify'  => "Service[#{_service_name}]",
+          'require' => 'File[vmtools_conf]',
+        })
+      end
+    end
+  end
+
+  platforms.select { |name, _hash| name =~ /(CentOS 7|RedHat 7|SLES 12|OpenSuSE 12|Ubuntu 14)/ }.each do |platform, v|
+    describe "on #{platform}" do
+      let(:facts) do
+        {
+          :osfamily               => v[:osfamily],
+          :operatingsystem        => v[:operatingsystem],
+          :operatingsystemrelease => v[:operatingsystemrelease],
+          :architecture           => v[:architecture],
+          :kernelrelease          => v[:kernelrelease],
+          :lsbdistcodename        => v[:lsbdistcodename],
+          :virtual                => 'vmware',
+          :vmware_has_x           => 'false',
+          :lsbdistid              => 'ubuntu',  # needed for apt
+          :lsbdistcodename        => 'precise', # needed for apt
+        }
+      end
+
+      context 'with manage_repo set to valid boolean true' do
+        let(:params) { { :manage_repo => true } }
+
+        case v[:operatingsystem]
+        when 'RedHat', 'CentOS'
+          it { should contain_yumrepo('vmware-osps') }
+          it { should_not contain_class('zyyprepo')}
+          it { should_not contain_zypprepo('vmware-osps') }
+          it { should_not contain_class('apt')}
+          it { should_not contain_apt__key('vmware') }
+          it { should_not contain_apt__source('vmware-osps') }
+        when 'SLED', 'SLES', 'OpenSuSE'
+          it { should_not contain_yumrepo('vmware-osps') }
+          it { should contain_class('zypprepo')}
+          it { should contain_zypprepo('vmware-osps') }
+          it { should_not contain_class('apt')}
+          it { should_not contain_apt__key('vmware') }
+          it { should_not contain_apt__source('vmware-osps') }
+        when 'Ubuntu'
+          it { should_not contain_yumrepo('vmware-osps') }
+          it { should_not contain_class('zyyprepo')}
+          it { should_not contain_zypprepo('vmware-osps') }
+          it { should contain_class('apt')}
+          it { should contain_apt__key('vmware') }
+          it { should contain_apt__source('vmware-osps') }
+        end
+
+        context 'when repo_base_url set to valid string http://spec.test/packages/esx' do
+          let(:params) { { :manage_repo => true, :repo_base_url => 'http://spec.test/packages/esx' } }
+
+          case v[:operatingsystem]
+          when 'RedHat', 'CentOS'
+            it { should contain_yumrepo('vmware-osps').with_baseurl(%r{^http://spec.test/packages/esx/}) }
+          when 'SLED', 'SLES', 'OpenSuSE'
+            it { should contain_zypprepo('vmware-osps').with_baseurl(%r{^http://spec.test/packages/esx/}) }
+          when 'Ubuntu'
+            it { should contain_apt__source('vmware-osps').with_location(%r{^http://spec.test/packages/esx/}) }
+          end
+        end
+
+        context 'when esx_version set to valid string 6.0' do
+          let(:params) { { :manage_repo => true, :esx_version => '6.0' } }
+
+          case v[:operatingsystem]
+          when 'RedHat', 'CentOS'
+            it { should contain_yumrepo('vmware-osps').with_baseurl(%r{^http://packages.vmware.com/tools/esx/6.0/}) }
+          when 'SLED', 'SLES', 'OpenSuSE'
+            it { should contain_zypprepo('vmware-osps').with_baseurl(%r{^http://packages.vmware.com/tools/esx/6.0/sles#{v[:operatingsystemrelease].gsub(/\./, 'sp')}/}) }
+          when 'Ubuntu'
+            it { should contain_apt__source('vmware-osps').with_location(%r{^http://packages.vmware.com/tools/esx/6.0}) }
+          end
+        end
+      end
+    end
+  end
+
+  describe 'on a KVM hypervisor' do
+    let(:facts) { [default_facts, { :virtual => 'kvm' }].reduce(:merge) }
+
+    it { should compile.with_all_deps }
+    it { should contain_class('vmware')}
+    it { should have_resource_count(0) }
+  end
+
+  describe 'on a physical machine' do
+    let(:facts) { [default_facts, { :virtual => 'physical' }].reduce(:merge) }
+
+    it { should compile.with_all_deps }
+    it { should contain_class('vmware')}
+    it { should have_resource_count(0) }
+  end
+end
+
+
+# old tests
+describe 'vmware' do
+  let(:default_facts) do
+    {
       :virtual                => 'vmware',
       :vmware_has_x           => 'false',
       :operatingsystem        => 'RedHat',
